8d1613307e3d3b73f42587238aa062e138f0efb4
Timm Rohweder <trohwede@mail.uni-mannheim.de>
2023-09-17 21:55:59 +0200

    cleaned and added attribute projection

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ import seaborn as sns
 from scipy import stats as st
 from scipy.linalg import svd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
 
+from scipy.stats import norm
 warnings.filterwarnings("ignore", category=matplotlib.MatplotlibDeprecationWarning)
 
 
@@ -53,25 +55,41 @@ def data_visualisation(data):
 
 
     # plot boxplots/distribution of features
-    plt.boxplot(data, labels=data.columns)
+    plt.figure(figsize=(10, 8))
+    plt.boxplot((data - data.mean()) / data.std() , labels=data.columns)
     plt.title("Boxplots of all Features")
     plt.xlabel("Features")
     plt.ylabel("Data values")
+    plt.xticks(rotation=90)
     plt.show()
 
-    n_bins = 25
-    fig, ax = plt.subplots(2, int(np.ceil(len(data.columns) / 2)))
-    plt.figure().set_figheight(10)
-    plt.figure().set_figwidth(20)
-    fig.tight_layout()
-    for col_id in range(len(data.columns)):
-        if col_id < int(np.ceil(len(data.columns) / 2)):
-            ax[0, col_id].hist(data.iloc[:, col_id], bins=n_bins)
-            ax[0, col_id].set_title(data.columns[col_id])
-        if col_id >= int(np.ceil(len(data.columns) / 2)):
-            ax[1, col_id - int(np.ceil(len(data.columns) / 2))].hist(data.iloc[:, col_id], bins=n_bins)
-            ax[1, col_id - int(np.ceil(len(data.columns) / 2))].set_title(data.columns[col_id])
+    #n_bins = 25
+    #fig, ax = plt.subplots(2, int(np.ceil(len(data.columns) / 2)))
+    #plt.figure().set_figheight(10)
+    #plt.figure().set_figwidth(20)
+    #fig.tight_layout()
+    #for col_id in range(len(data.columns)):
+    #    if col_id < int(np.ceil(len(data.columns) / 2)):
+    #        ax[0, col_id].hist(data.iloc[:, col_id], bins=n_bins)
+    #        ax[0, col_id].set_title(data.columns[col_id])
+    #    if col_id >= int(np.ceil(len(data.columns) / 2)):
+    #        ax[1, col_id - int(np.ceil(len(data.columns) / 2))].hist(data.iloc[:, col_id], bins=n_bins)
+    #        ax[1, col_id - int(np.ceil(len(data.columns) / 2))].set_title(data.columns[col_id])
+    #plt.show()
+
+    # Set up the figure size and grid layout
+    plt.figure(figsize=(15, 12))
+    sns.set_style("whitegrid")
+
+    # Plot histograms for each column
+    for i, column in enumerate(data.columns.drop("season"), 1):
+        plt.subplot(3, 4, i)
+        sns.histplot(data[column], kde=True)
+        plt.title(f'Distribution of {column}')
+        plt.tight_layout()
     plt.show()
+
+
 
     # show correlations
     plt.figure(figsize=(10,8))
@@ -82,6 +100,8 @@ def data_visualisation(data):
     plt.tight_layout
     plt.show()
 
+    #with open(os.path.join(os.getcwd(), "data_measures.txt"), 'w') as f:
+    #    f.write(data.corr().to_string())
 
 def pca(data):
     ### transform data ###
@@ -100,6 +120,8 @@ def pca(data):
 
     # Compute variance explained by principal components
     rho = (S * S) / (S * S).sum()
+
+
 
     plt.figure()
     plt.plot(range(1, len(rho) + 1), rho, 'x-')
@@ -134,6 +156,7 @@ def pca(data):
     plt.show()
 
 
+
 def pca_analysis(data):
     # Temp - IBH IBT
     # Season - Temp vis
@@ -150,38 +173,57 @@ def pca_analysis(data):
     ax[1].set_ylabel('IBT')
     plt.show()
 
+    ### Mapping season to temperature ####
     # Set up the plot
     plt.figure(figsize=(10, 6))
     colors = ['green', "red", "blue", "orange"]
-    # Horizontal line for 1D representation
     plt.axhline(y=1, color='grey', linestyle='--', lw=0.5)
-
-    # Plot data with different symbols for each season on the horizontal line
     for i, row in data.iterrows():
         plt.scatter(row['temp'], 1, color=colors[data["season"][i]])
-
-    # Set plot details
     plt.title("Temperature with Season Symbols")
     plt.xlabel("Temperature (°C)")
     plt.yticks([])  # Hide y-ticks as it's a 1D plot
     plt.legend()
     plt.grid(True, which='both', linestyle='--', linewidth=0.5, axis='x')
-
-    # Display the plot
     plt.tight_layout()
     plt.show()
 
-    f = plt.figure()
-    plt.title('NanoNose data')
 
+    ###  Mapping season to temperature and visibility ###
     for c in range(4):
         # select indices belonging to class c:
         class_mask = data["season"] == c
         plt.plot(data["temp"][class_mask], data["vis"][class_mask], 'o', alpha=.3)
 
-    #    plt.legend(data["season"])
-    #   plt.xlabel(data["temp"])
-    #  plt.ylabel(data["vis"])
+    plt.legend(data["season"])
+    #plt.xlabel(data["temp"])
+    #plt.ylabel(data["vis"])
+    plt.show()
+
+
+    # Show projection of data onto principal components
+    mean = data.mean()
+    std = data.std()
+    data_normalized = (data - mean) / std
+
+    pca = PCA()
+    pca.fit(data_normalized.drop(["doy", "season"]))
+    num_components = 5
+
+    fig, ax = plt.subplots(figsize=(14, 8))
+
+    for i in range(num_components):
+        ax.plot(data.columns, pca.components_[i], label=f'Component {i + 1}', marker='o')
+
+    for i in range(num_components):
+        print(pca.components_[i])
+
+    ax.set_xticks(data.columns)
+    ax.set_xticklabels(data.columns, rotation=45)
+    ax.set_ylabel('Loading')
+    ax.set_title('PCA Component Loadings for Each Feature')
+    ax.legend()
+    ax.grid(True)
     plt.show()
 
 
@@ -208,10 +250,15 @@ def main():
             data["season"][row] = 3
 
     #inspect_data(data)
-    data_visualisation(data)
-    # basic_statistical_metrics(data)
+    #data_visualisation(data)
     #pca(data)
-    # pca_analysis(data)
+    pca_analysis(data)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
